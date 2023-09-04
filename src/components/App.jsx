25f5786d810b6450914b6c0ec6f186a923411f0b
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-09-04 15:47:41 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,27 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
-import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const newContact = { id: nanoid(), name, number };
@@ -39,14 +47,7 @@ class App extends Component {
     );
 
     return (
-      <div
-        style={{
-          margin: 'auto',
-          textAlign:
-            'center',
-          
-        }}
-      >
+      <div style={{ margin: 'auto', textAlign: 'center' }}>
         <h1>Phonebook</h1>
         <ContactForm addContact={this.addContact} contacts={contacts} />
         <h2>Contacts</h2>
